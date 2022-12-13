9c379f08e8c63952f5110182dec215f914e4ce2b
MMachado <marcialmachado0522@gmail.com>
2022-12-13 18:29:05 -0500

    Changed Character to GameCharacter
    This was to avoid the Character wrapper class. I've also included attributes and a constructor.

---
+++ src/main/java/entities/assets/GameCharacter.java
@@ -0,0 +1,45 @@
+package entities.assets;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * An abstract class for any animate asset in the game.
+ * Created: 12.13.2022
+ * Created: 12.13.2022
+ *
+ * @author MMachado05
+ */
+public abstract class GameCharacter {
+    /* ********** *\
+    *  Attributes  *
+    \* ********** */
+    protected final String name;
+    protected String description;
+
+    protected int maxHealth;
+    protected int health;
+
+    protected int level;
+
+    protected final List<Item> inventory;
+    
+    /* ************ *\
+    *  Constructors  *
+    \* ************ */
+    /**
+     * Constructs a default GameCharacter object
+     * 
+     * @param name This character's name
+     * @param description This character's description
+     * @param maxHealth This character's default max health
+     */
+    public GameCharacter(String name, String description, int maxHealth) {
+        this.name = name;
+        this.description = description;
+        this.maxHealth = maxHealth;
+        this.health = maxHealth;
+        this.level = 1;
+        this.inventory = new ArrayList<>();
+    } 
+}
