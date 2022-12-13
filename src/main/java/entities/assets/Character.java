694240b8e8179a1c0b0ce123a86aabfce60ce1de
MMachado <marcialmachado0522@gmail.com>
2022-12-13 14:39:33 -0500

    Created Item and Character abstract classes

---
+++ src/main/java/entities/assets/Character.java
@@ -0,0 +1,11 @@
+package entities.assets;
+
+/**
+ * An abstract class for any animate asset in the game.
+ * Created: 12.13.2022
+ * Created: 12.13.2022
+ *
+ * @author MMachado05
+ */
+public abstract class Character {
+}
---
+++ src/main/java/entities/assets/Item.java
@@ -0,0 +1,11 @@
+package entities.assets;
+
+/**
+ * An abstract class for any inanimate asset in the game.
+ * Created: 12.13.2022
+ * Created: 12.13.2022
+ *
+ * @author MMachado05
+ */
+public abstract class Item {
+}
